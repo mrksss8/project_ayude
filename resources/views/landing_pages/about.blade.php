fc14be5663066e30e4098391482e992ee15d541f
JudahPraise <judahpraiseocampo@gmail.com>
2022-04-25 23:14:26 +0800

    Home and about added

--- resources/views/landing_pages/about.blade.php
+++ resources/views/landing_pages/about.blade.php
@@ -5,9 +5,10 @@
         <div class="overlay"></div>
         <div class="container">
           <div class="row no-gutters slider-text align-items-center justify-content-center" data-scrollax-parent="true">
-            <div class="col-md-7 ftco-animate text-center" data-scrollax=" properties: { translateY: '70%' }">
-               <p class="breadcrumbs" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }"><span class="mr-2"><a href="index.html">Home</a></span> <span>About</span></p>
-              <h1 class="mb-3 bread" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }">About Us</h1>
+            <div class="col-md-10 ftco-animate text-center" data-scrollax=" properties: { translateY: '70%' }">
+               {{-- <p class="breadcrumbs" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }"><span class="mr-2"><a href="index.html">Home</a></span> <span>About</span></p> --}}
+              <h1 class="mb-3 bread" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }">Mission & Vision</h1>
+              <p class="mb-5" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }">The key mission of Ayude is to provide for the poorest of the poor – more specifically the numerous (street) children in less-developed countries. We try to improve the pitiful life circumstances of these children and their families by meeting their physical, mental and spiritual needs. </p>
             </div>
           </div>
         </div>
@@ -18,103 +19,113 @@
         <div class="container">
             <div class="row d-flex">
                 <div class="col-md-6 d-flex ftco-animate">
-                    <div class="img img-about align-self-stretch" style="background-image: url({{ asset('images/bg_3.jpg') }}); width: 100%;"></div>
+                    <div class="img img-about align-self-stretch" style="background-image: url(https://www.ayude.be/wp-content/uploads/2021/05/FB_IMG_1620086337227_edit_349678056188307-1.jpg); width: 100%;"></div>
                 </div>
                 <div class="col-md-6 pl-md-5 ftco-animate">
-                    <h2 class="mb-4">Welcome to Welfare Stablished Since 1898</h2>
-                    <p>The Big Oxmox advised her not to do so, because there were thousands of bad Commas, wild Question Marks and devious Semikoli, but the Little Blind Text didn’t listen. She packed her seven versalia, put her initial into the belt and made herself on the way.</p>
-                    <p>On her way she met a copy. The copy warned the Little Blind Text, that where it came from it would have been rewritten a thousand times and everything that was left from its origin would be the word "and" and the Little Blind Text should turn around and return to its own, safe country. But nothing the copy said could convince her and so it didn’t take long until a few insidious Copy Writers ambushed her, made her drunk with Longe and Parole and dragged her into their agency, where they abused her for their.</p>
+                    <h2 class="mb-4">Our Mission</h2>
+                    <p>
+                        1. Ayude supports local projects against structural poverty in southern less-developed countries.
+                        <br>
+                        <span>
+                            <ul>
+                                <li>Ayude, with the cooperation of partner organisations in third world countries,fights for improvement of life circumstances of (street) children by addressing the fundamental causes and consequences of poverty.</li>
+                                <li>We accompany local partners in their endeavors when setting up durable livelihood projects, e.g. such as granting microcredit, alternative learning methods for unskilled adults and parents etcetera…</li>
+                                <li>The partner organisations of Ayude provide help to street- and underprivileged children and their communities, without making any distinction in religion, race, gender, age or political beliefs.</li>
+                            </ul>
+                        </span>
+                        <br>
+                        2. We try to sensitize the people in Belgium and the West as much as possible for the North-South divide and to raise funds for projects aimed at relieving the plight of poor (street) children in the less-developed countries.
+                    </p>
                 </div>
             </div>
         </div>
     </section>
 
-    <section class="ftco-counter ftco-intro ftco-intro-2" id="section-counter">
+    <section class="ftco-section">
         <div class="container">
-            <div class="row no-gutters">
-                <div class="col-md-5 d-flex justify-content-center counter-wrap ftco-animate">
-            <div class="block-18 color-1 align-items-stretch">
-              <div class="text">
-                  <span>Served Over</span>
-                <strong class="number" data-number="1432805">0</strong>
-                <span>Children in 190 countries in the world</span>
-              </div>
-            </div>
-          </div>
-          <div class="col-md d-flex justify-content-center counter-wrap ftco-animate">
-            <div class="block-18 color-2 align-items-stretch">
-              <div class="text">
-                  <h3 class="mb-4">Donate Money</h3>
-                  <p>Even the all-powerful Pointing has no control about the blind texts.</p>
-                  <p><a href="#" class="btn btn-white px-3 py-2 mt-2">Donate Now</a></p>
-              </div>
-            </div>
-          </div>
-          <div class="col-md d-flex justify-content-center counter-wrap ftco-animate">
-            <div class="block-18 color-3 align-items-stretch">
-              <div class="text">
-                  <h3 class="mb-4">Be a Volunteer</h3>
-                  <p>Even the all-powerful Pointing has no control about the blind texts.</p>
-                  <p><a href="#" class="btn btn-white px-3 py-2 mt-2">Be A Volunteer</a></p>
-              </div>
-            </div>
-          </div>
+            <div class="row d-flex">
+                <div class="col-md-6 pl-md-5 ftco-animate">
+                    <h2 class="mb-4">Our Vision</h2>
+                    <p>
+                        Ayude has as its vision to show as much as possible to underprivileged and less fortunate street children in this world that they too are important. Driven by our Christian world view we do not make a distinction in race, religion or political belief. For Ayude every child is unique and priceless in its worth.                     
+                        <br>
+                        <br>
+                        Through our programs we prevent that children, who are usually underprivileged and neglected, often leave their families out of necessity and consequently end up along the streets of the city into criminal environments like child prostitution and drug trafficking. We believe that sustainable development education is the key to get these children out of the vicious cycle of poverty and consequently to develop their self-esteem and self-confidence. Principally we are working towards a sustainable improvement of living circumstances and conditions for themselves, their families and communities. Hereby they can, with renewed hope and zest for life, provide in their own livelihood and learn to take responsibility inside the micro society (community) around them.
+                    </p>
+                </div>
+                <div class="col-md-6 d-flex ftco-animate">
+                    <div class="img img-about align-self-stretch" style="background-image: url(https://www.ayude.be/wp-content/uploads/2021/02/FB_IMG_1613202447687_edit_89341461518658-2-450x600.jpg); width: 100%;"></div>
+                </div>
             </div>
         </div>
     </section>
 
     <section class="ftco-section bg-light">
-      <div class="container">
-          <div class="row justify-content-center mb-5 pb-3">
-          <div class="col-md-7 heading-section ftco-animate text-center">
-            <h2 class="mb-4">Latest Donations</h2>
-            <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-          </div>
+        <div class="container">
+            <div class="row justify-content-center mb-5 pb-3">
+                <div class="col-md-12 heading-section ftco-animate text-center">
+                  <p>After 20 years we can say that our approach is paying off! Today the children of 1995 who have been helped are working as certified accountants, psychologists, social workers etc. And many of them are now in turn actively involved in our projects because they experience the value and fruit of the help they have experienced themselves before. , want to pass on to the street children in their own living environment. The commitment of professional and well-trained people (teachers, doctor (s), dentists, nurses, etc.) is combined from the outset with the active commitment of all those directly involved, the children, their parents and all members of the communities.</p>
+                </div>
+            </div>
+            <div class="row d-flex justify-content-center">
+                <div class="col-lg-4 d-flex mb-sm-4 ftco-animate">
+                    <div class="staff p-5">
+                        <div class="container d-flex flex-column align-items-center">
+                            <div class="img" style="background-image: url({{ asset('images/person_1.jpg') }});"></div>
+                            <div class="d-flex flex-column align-items-center">
+                                <h3>Baltazar Mangaliman</h3>
+                                <span class="position text-center">Senior Audit Manager at KPMG UK</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 d-flex mb-sm-4 ftco-animate">
+                    <div class="staff p-5">
+                        <div class="container d-flex flex-column align-items-center">
+                            <div class="img" style="background-image: url({{ asset('images/person_2.jpg') }});"></div>
+                            <div class="d-flex flex-column align-items-center">
+                                <h3>Lyza Pagapulan</h3>
+                                <span class="position text-center">Faculty Designated as Research Extension Program Coordinator and Administrative Officer at Polytechnic University of the Philippines Calauan Campus</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 d-flex mb-sm-4 ftco-animate">
+                    <div class="staff p-5">
+                        <div class="container d-flex flex-column align-items-center">
+                            <div class="img" style="background-image: url({{ asset('images/person_3.jpg') }});"></div>
+                            <div class="d-flex flex-column align-items-center">
+                                <h3>Mark Elson Leones</h3>
+                                <span class="position text-center">Inventory Supervisor at Chemical Research Products Industrial Sales Inc. Water Treatment Facility</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row d-flex justify-content-center">
+                <div class="col-lg-4 d-flex mb-sm-4 ftco-animate">
+                    <div class="staff p-5">
+                        <div class="container d-flex flex-column align-items-center">
+                            <div class="img" style="background-image: url({{ asset('images/person_4.jpg') }});"></div>
+                            <div class="d-flex flex-column align-items-center">
+                                <h3>Liesel Pagapulan</h3>
+                                <span class="position text-center">HR Manager at Care Techniques Saudi Arabia</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 d-flex mb-sm-4 ftco-animate">
+                    <div class="staff p-5">
+                        <div class="container d-flex flex-column align-items-center">
+                            <div class="img" style="background-image: url({{ asset('images/person_5.jpg') }});"></div>
+                            <div class="d-flex flex-column align-items-center">
+                                <h3>Michelle Makinano</h3>
+                                <span class="position text-center">Administration and Human Resource Officer as SMSG Communication Ltd. Software Development</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="row">
-            <div class="col-lg-4 d-flex mb-sm-4 ftco-animate">
-                <div class="staff">
-                    <div class="d-flex mb-4">
-                        <div class="img" style="background-image: url({{ asset('images/person_1.jpg') }});"></div>
-                        <div class="info ml-4">
-                            <h3><a href="teacher-single.html">Ivan Jacobson</a></h3>
-                            <span class="position">Donated Just now</span>
-                            <div class="text">
-                                <p>Donated <span>$300</span> for <a href="#">Children Needs Food</a></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 d-flex mb-sm-4 ftco-animate">
-                <div class="staff">
-                    <div class="d-flex mb-4">
-                        <div class="img" style="background-image: url({{ asset('images/person_2.jpg') }});"></div>
-                        <div class="info ml-4">
-                            <h3><a href="teacher-single.html">Ivan Jacobson</a></h3>
-                            <span class="position">Donated Just now</span>
-                            <div class="text">
-                                <p>Donated <span>$150</span> for <a href="#">Children Needs Food</a></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 d-flex mb-sm-4 ftco-animate">
-                <div class="staff">
-                    <div class="d-flex mb-4">
-                        <div class="img" style="background-image: url({{ asset('images/person_3.jpg') }});"></div>
-                        <div class="info ml-4">
-                            <h3><a href="teacher-single.html">Ivan Jacobson</a></h3>
-                            <span class="position">Donated Just now</span>
-                            <div class="text">
-                                <p>Donated <span>$250</span> for <a href="#">Children Needs Food</a></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-      </div>
     </section>
 @endsection
